006890049cace3079cf1c90ae5d30060bc786df8
randyhr318 <74457618+randyhr318@users.noreply.github.com>
2021-02-10 19:25:40 -0600

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,119 @@
+let resumeData = {
+    // "imagebaseurl":"https://randyhr318.github.io/",
+    "name": "Randy Roberson",
+    "role": "Fullstack Developer. ",
+    "linkedinId":"https://www.linkedin.com/in/randy-roberson-756a851bb/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "My mission first and foremost is to provide my clients with professional, functional full stack web development that will meet their needs to grow and advance their businesses.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/randy-roberson-756a851bb/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/randyhr318ra",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"twitter url",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am currently attending Tech Talent South foundational Java bootcamp.",
+    "address":"Shreveport",
+    // "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"Louisiana Technical College",
+        "specialization":"HVAC",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2006",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Benton High School",
+        // "specialization":"Some specialization",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2004",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Tech Talent South",
+        "specialization":"Full Stack Development",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2021",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Mid South Fire Solutions",
+        "specialization":"Fire alarm service superintendent",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2020",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Lockbusters of Louisiana",
+        "specialization":"Locksmithing and access control",
+        "MonthOfLeaving":"Oct",
+        "YearOfLeaving":"2017",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Java"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
